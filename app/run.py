55dfa894b4938861cef4d39e35da3a8dd361a9ff
Amilcar06 <93298327+Amilcar06@users.noreply.github.com>
2025-01-28 00:29:52 -0400

    Update README.md

--- app/run.py
+++ app/run.py
@@ -1,10 +1,12 @@
-from flask import Flask,flash
+from flask import Flask, flash
 from controllers import header_controller, auth_controller
-from controllers import vehiculo_controller,estado_vehiculo_controller,reserva_controller, ingresos_controller, seguro_controller,aseguradora_controller,routes, empleado_controller, reporte_controller
+from controllers import vehiculo_controller, estado_vehiculo_controller, reserva_controller
+from controllers import ingresos_controller, seguro_controller, aseguradora_controller, routes
+from controllers import empleado_controller, reporte_controller
 
 app = Flask(__name__)
 
-app.secret_key="mi_clave_secreta"
+app.secret_key = "mi_clave_secreta"
 
 app.register_blueprint(vehiculo_controller.vehiculo_bp)
 app.register_blueprint(estado_vehiculo_controller.est_vehiculo_bp)
@@ -19,7 +21,5 @@ app.register_blueprint(empleado_controller.empleado_persona_bp)
 app.register_blueprint(auth_controller.auth_bp)
 
 if __name__ == "__main__":
-      app.run(port=5001) 
-      app.run(debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
